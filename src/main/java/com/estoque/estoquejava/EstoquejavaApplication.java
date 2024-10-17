7c86c9563de9f7e8bceb33e02a74d6e0b9058987
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2024-10-17 15:36:19 -0300

    first commit

---
+++ src/main/java/com/estoque/estoquejava/EstoquejavaApplication.java
@@ -0,0 +1,13 @@
+package com.estoque.estoquejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquejavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estoque/estoquejava/EstoquejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.estoquejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
